80a5c5d2eda3469c5dff99b34765a31265e6c894
Ansari Mohd Asad <ansarimohdasad308@gmail.com>
2026-09-09 23:25:48 +0530

    abstract class example

---
+++ Java/abstraction_eg.java
@@ -0,0 +1,34 @@
+package TNS;
+
+abstract class Animal{
+abstract void sound();
+void eat() {
+	System.out.println("Hello from non abstract method");
+}
+}
+class dog extends Animal{
+        @Override
+	void sound() {
+		System.out.println("Dog Barks");
+	}
+}
+class cat extends Animal{
+        @Override
+	void sound() {
+		System.out.println("Cat not Barks");
+	}
+}
+public class abstraction_eg {
+
+	public static void main(String[] args) {
+		System.out.println("Abstract Class Example!!");
+		Animal obj1=new dog();
+		Animal obj2=new cat();
+		
+		obj1.sound();
+		obj2.sound();
+        obj1.eat();
+
+	}
+
+}
